760f3d7da63dfc4cb08c5982139462569994b1f2
Chris Alejandro Palabay <2191948@slu.edu.ph>
2021-06-02 15:59:50 +0800

    Added simple nav bar

--- landing.php
+++ landing.php
@@ -1,20 +1,63 @@
-<!doctype html>
+<html>
 <html lang="en">
   <head>
-    <title>Title</title>
+    
+  <title>GDC</title>
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <style>
+      #section1{
+      height: 100%;
+      background: #D6EAF8;
+      display: inline-block;
+      width: 100%;
+      }
+      #section2{
+      height: 100%;
+      background: #D6EAF8;
+      display: inline-block;
+      width: 100%;
+      }
+      #section3{
+      height: 100%;
+      background: #D6EAF8;
+      display: inline-block;
+      width: 100%;
+      }
+    </style>
   </head>
+  
+
   <body>
+      <div>
+        <nav>
+         <ul>
+            <li><a href="#section1">Home</a></li>
+            <li><a href="#section2">Appointments</a></li>
+            <li><a href="#section3">About Us</a></li>
+         </ul>
+        </nav>
+      </div>
+
+      <div id="section1">
+          <h1>Home</h1>
+      </div>
+      <div id="section2">
+          <h1>Appointments</h1>
+      </div>
+      <div id="section3">
+          <h1>About us</h1>
+      </div>
+        
 
 
 
-     <h1>Test</h1>
-      
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
