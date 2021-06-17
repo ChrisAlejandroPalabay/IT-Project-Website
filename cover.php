01ec329de676ea7d34bce5488d66612eba6091fb
Chris Alejandro Palabay <2191948@slu.edu.ph>
2021-06-17 10:19:27 +0800

    Nav bar configuration

--- cover.php
+++ cover.php
@@ -8,56 +8,12 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/style.css">  
   </head>
   <body>
-  <!-- Nav Bar -->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-light">
-  <a class="navbar-brand" href="#home"><img src="images/logo.png" alt="logo" width="30px">
-  <b>Gabrillo Dental Clinic</b>
-  </a>
-  <div class="collapse navbar-collapse">
-    <ul class="navbar-nav">
-      <div class="tab1">
-        <li class="nav-item">
-          <a class="nav-link" href="#home">Home</a>
-        </li>
-      </div>
-      <div class="tab2">
-        <li class="nav-item">
-          <a class="nav-link" href="#appointments">Appointments</a>
-        </li>
-      </div>
-      <div class="tab3">
-        <li class="nav-item">
-          <a class="nav-link" href="#aboutus">About Us</a>
-        </li>
-      </div>
-    </ul>
-  </div>
-</nav>
-
-<!-- Home Content -->
-<div id="home">
-<h1>home</h1>
-<img src="images/vec1.png" alt="vec1" width="500">
-</div>
-<div id="appointments">
-<!-- Appointments Content -->
-<h1>Appointments</h1>
-<img src="images/Vector2.jpeg" alt="vec2" width="500">
-</div>
-<!-- AboutUsS Content -->
-<div id="aboutus">
-<h1>About US</h1>
-<img src="images/trees.jpg" alt="vec3" width="500">
-</div>
       
 
-
-
-
-
+  <h1>WElcome to THE gabrillo dental clinic!</h1>
+  <a href="home.php">click to to go home page</a>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
