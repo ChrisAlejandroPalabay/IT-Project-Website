023cfea7681a44c0609d6ac04f142b9cb97735e7
Chris Alejandro Palabay <2191948@slu.edu.ph>
2021-06-19 21:15:40 +0800

    Repositioned SVG and add content

--- home.php
+++ home.php
@@ -9,6 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/style.css">  
+    <script src="svg-inject.min.js"></script>
   </head>
   <body>
   <!-- Nav Bar -->
@@ -35,26 +36,36 @@
   </div>
 </nav>
 <!--SGV Background --->
-
 <svg class="svg1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1019.5 924.73"><defs><style>.cls-1{opacity:0.15;}.cls-2{fill:#9f91da;}</style></defs><title>BIgC</title><g id="Layer_2" data-name="Layer 2"><g id="BIgg"><g class="cls-1"><path class="cls-2" d="M118.56,0,1019.5,1.12V714.49C914.06,842.85,754.09,924.73,575,924.73c-317.56,0-575-257.44-575-575A572.46,572.46,0,0,1,118.56,0Z"/></g></g></g></svg>
-<svg class="svg2 "  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 425 286"><defs><style>.cls-1{fill:#9f91da;opacity:0.15;}</style></defs><title>TopC</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_2-2" data-name="Layer 2"><path class="cls-1" d="M425,0V250.36A285.22,285.22,0,0,1,286.5,286C128.44,286,.27,158,0,0Z"/></g></g></svg>
-<svg class="svg3"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 353 573"><defs><style>.cls-1{fill:#9f91da;opacity:0.15;}</style></defs><title>bottomR</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_2-2" data-name="Layer 2"><path class="cls-1" d="M353,7.77V565.23A286.89,286.89,0,0,1,286.5,573C162.32,573,56.59,494,16.85,383.5A285.69,285.69,0,0,1,0,286.5C0,128.27,128.27,0,286.5,0A286.89,286.89,0,0,1,353,7.77Z"/></g></g></svg>
+<svg class="svg2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 425 286"><defs><style>.cls-1{fill:#9f91da;opacity:0.15;}</style></defs><title>TopC</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_2-2" data-name="Layer 2"><path class="cls-1" d="M425,0V250.36A285.22,285.22,0,0,1,286.5,286C128.44,286,.27,158,0,0Z"/></g></g></svg>
+<svg class="svg3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 353 573"><defs><style>.cls-1{fill:#9f91da;opacity:0.15;}</style></defs><title>bottomR</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_2-2" data-name="Layer 2"><path class="cls-1" d="M353,7.77V565.23A286.89,286.89,0,0,1,286.5,573C162.32,573,56.59,494,16.85,383.5A285.69,285.69,0,0,1,0,286.5C0,128.27,128.27,0,286.5,0A286.89,286.89,0,0,1,353,7.77Z"/></g></g></svg>
+<svg class="svg4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352.5 422"><defs><style>.cls-1{fill:#9f91da;opacity:0.15;}</style></defs><title>BottomL</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_2-2" data-name="Layer 2"><path class="cls-1" d="M352.5,211c0,116.53-94.47,211-211,211A210.21,210.21,0,0,1,0,367.52v-313A210.21,210.21,0,0,1,141.5,0C253.83,0,345.67,87.79,352.13,198.5Q352.51,204.71,352.5,211Z"/></g></g></svg>
+
 
 
 
 
 <!-- Home Content -->
 <div id="home">
-<img class="vector1" src="images/Vector1.png" alt="vec1" width="500">
+  <div class="row">
+    <div class="col-lg-5">
+      <img class="vector1"src="images/Vector1.png" alt="Vec1" width="500px">
+    </div>
+    <div class="col-lg">
+      <h2 class="text-primary">WELCOME TO GABRILLO DENTAL CLINIC</h2>
+      <p>Assign responsive-friendly margin or padding values to an element or a subset of its sides with shorthand classes. Includes support for individual properties, all properties, and vertical and horizontal properties. Classes are built from a default Sass map ranging from .25rem to 3rem.</p>
+    </div>
+  </div>
 </div>
+
 <div id="appointments">
 <!-- Appointments Content -->
-<img class="vector2" src="images/Vector2.png" alt="vec2" width="325" height="375">
+<img class="vector2" src="images/Vector2.png" alt="vec2" width="300px">
 </div>
+
 <!-- AboutUsS Content -->
 <div id="aboutus">
 <h1>About US</h1>
-<img src="images/trees.jpg" alt="vec3" width="500">
 </div>
       
 
