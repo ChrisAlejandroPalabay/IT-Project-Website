66359dd01b0ad57206581fdfea4d321de27d97f1
Chris Alejandro Palabay <2191948@slu.edu.ph>
2021-06-21 22:38:02 +0800

    Initial Website design and added more svg files

--- home.php
+++ home.php
@@ -1,82 +1,190 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/style.css">  
-    <script src="svg-inject.min.js"></script>
-  </head>
-  <body>
-  <!-- Nav Bar -->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-  <a class="navbar-brand" href="#home"><img src="images/logo.png" alt="logo" width="45px">
-  <b>GABRILLO DENTAL CLINIC</b>
-  </a>
-  <div class="collapse navbar-collapse">
-    <ul class="navbar-nav">
-      
-        <li>
-          <a class="color-me" href="#home"><b>HOME</b></a>
-        </li>
-    
-        <li>
-          <a class="color-me" href="#appointments"><b>APPOINTMENTS</b></a>
-        </li>
-        
-        <li>
-          <a class="color-me" href="#aboutus"><b>ABOUT US</b></a>
-        </li>
-      
-    </ul>
-  </div>
-</nav>
-<!--SGV Background --->
-<svg class="svg1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1019.5 924.73"><defs><style>.cls-1{opacity:0.15;}.cls-2{fill:#9f91da;}</style></defs><title>BIgC</title><g id="Layer_2" data-name="Layer 2"><g id="BIgg"><g class="cls-1"><path class="cls-2" d="M118.56,0,1019.5,1.12V714.49C914.06,842.85,754.09,924.73,575,924.73c-317.56,0-575-257.44-575-575A572.46,572.46,0,0,1,118.56,0Z"/></g></g></g></svg>
-<svg class="svg2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 425 286"><defs><style>.cls-1{fill:#9f91da;opacity:0.15;}</style></defs><title>TopC</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_2-2" data-name="Layer 2"><path class="cls-1" d="M425,0V250.36A285.22,285.22,0,0,1,286.5,286C128.44,286,.27,158,0,0Z"/></g></g></svg>
-<svg class="svg3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 353 573"><defs><style>.cls-1{fill:#9f91da;opacity:0.15;}</style></defs><title>bottomR</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_2-2" data-name="Layer 2"><path class="cls-1" d="M353,7.77V565.23A286.89,286.89,0,0,1,286.5,573C162.32,573,56.59,494,16.85,383.5A285.69,285.69,0,0,1,0,286.5C0,128.27,128.27,0,286.5,0A286.89,286.89,0,0,1,353,7.77Z"/></g></g></svg>
-<svg class="svg4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352.5 422"><defs><style>.cls-1{fill:#9f91da;opacity:0.15;}</style></defs><title>BottomL</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_2-2" data-name="Layer 2"><path class="cls-1" d="M352.5,211c0,116.53-94.47,211-211,211A210.21,210.21,0,0,1,0,367.52v-313A210.21,210.21,0,0,1,141.5,0C253.83,0,345.67,87.79,352.13,198.5Q352.51,204.71,352.5,211Z"/></g></g></svg>
-
-
-
-
-
-<!-- Home Content -->
-<div id="home">
-  <div class="row">
-    <div class="col-lg-5">
-      <img class="vector1"src="images/Vector1.png" alt="Vec1" width="500px">
-    </div>
-    <div class="col-lg">
-      <h2 class="text-primary">WELCOME TO GABRILLO DENTAL CLINIC</h2>
-      <p>Assign responsive-friendly margin or padding values to an element or a subset of its sides with shorthand classes. Includes support for individual properties, all properties, and vertical and horizontal properties. Classes are built from a default Sass map ranging from .25rem to 3rem.</p>
-    </div>
-  </div>
-</div>
-
-<div id="appointments">
-<!-- Appointments Content -->
-<img class="vector2" src="images/Vector2.png" alt="vec2" width="300px">
-</div>
-
-<!-- AboutUsS Content -->
-<div id="aboutus">
-<h1>About US</h1>
-</div>
-      
-
-
-
-
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+   <head>
+      <title>Title</title>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="styles/style.css">
+      <link rel="stylesheet" href="styles/svg.css">
+      <script src="svg-inject.min.js"></script>
+   </head>
+   <body>
+      <!-- Nav Bar -->
+      <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+         <a class="navbar-brand" href="#home"><img src="images/logo.png" alt="logo" width="45px">
+         <b>GABRILLO DENTAL CLINIC</b>
+         </a>
+         <div class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+               <li>
+                  <a class="color-me" href="#home"><b>HOME</b></a>
+               </li>
+               <li>
+                  <a class="color-me" href="#appointments"><b>APPOINTMENTS</b></a>
+               </li>
+               <li>
+                  <a class="color-me" href="#aboutus"><b>ABOUT US</b></a>
+               </li>
+            </ul>
+         </div>
+      </nav>
+      <!--SGV Background --->
+      <svg class="svg1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1019.5 924.73">
+         <defs>
+            <style>.cls-1{opacity:0.15;}.cls-2{fill:#9f91da;}</style>
+         </defs>
+         <title>BIgC</title>
+         <g id="Layer_2" data-name="Layer 2">
+            <g id="BIgg">
+               <g class="cls-1">
+                  <path class="cls-2" d="M118.56,0,1019.5,1.12V714.49C914.06,842.85,754.09,924.73,575,924.73c-317.56,0-575-257.44-575-575A572.46,572.46,0,0,1,118.56,0Z"/>
+               </g>
+            </g>
+         </g>
+      </svg>
+      <svg class="svg2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 425 286">
+         <defs>
+            <style>.cls-1{fill:#9f91da;opacity:0.15;}</style>
+         </defs>
+         <title>TopC</title>
+         <g id="Layer_2" data-name="Layer 2">
+            <g id="Layer_2-2" data-name="Layer 2">
+               <path class="cls-1" d="M425,0V250.36A285.22,285.22,0,0,1,286.5,286C128.44,286,.27,158,0,0Z"/>
+            </g>
+         </g>
+      </svg>
+      <svg class="svg3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352.5 422">
+         <defs>
+            <style>.cls-1{fill:#9f91da;opacity:0.15;}</style>
+         </defs>
+         <title>BottomL</title>
+         <g id="Layer_2" data-name="Layer 2">
+            <g id="Layer_2-2" data-name="Layer 2">
+               <path class="cls-1" d="M352.5,211c0,116.53-94.47,211-211,211A210.21,210.21,0,0,1,0,367.52v-313A210.21,210.21,0,0,1,141.5,0C253.83,0,345.67,87.79,352.13,198.5Q352.51,204.71,352.5,211Z"/>
+            </g>
+         </g>
+      </svg>
+      <svg class="svg4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 909 1566">
+         <defs>
+            <style>.cls-1{fill:#9f91da;opacity:0.15;}</style>
+         </defs>
+         <title>RGroupBig</title>
+         <g id="Layer_2" data-name="Layer 2">
+            <g id="Layer_2-2" data-name="Layer 2">
+               <path class="cls-1" d="M909,376.26v738.48q-15.33,15.77-31.94,30.23a527.37,527.37,0,0,1-345.57,130h-2C237.07,1275,0,1037.93,0,745.5S237.07,216,529.5,216A528.29,528.29,0,0,1,909,376.26Z"/>
+               <path class="cls-1" d="M909,23.13V401.87A211.54,211.54,0,0,1,812.5,425c-114.62,0-208-90.75-212.34-204.32q-.16-4.06-.16-8.18C600,95.14,695.14,0,812.5,0A211.54,211.54,0,0,1,909,23.13Z"/>
+               <circle class="cls-1" cx="705.5" cy="1368.5" r="197.5"/>
+               <path class="cls-1" d="M908,1173.36v123.28a105,105,0,0,1-189.86-56.23c-.09-1.79-.14-3.59-.14-5.41a105,105,0,0,1,190-61.64Z"/>
+            </g>
+         </g>
+      </svg>
+      <svg class="svg5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 266 634">
+         <defs>
+            <style>.cls-1{fill:#9f91da;opacity:0.15;}</style>
+         </defs>
+         <title>LowestRight</title>
+         <g id="Layer_2" data-name="Layer 2">
+            <g id="Layer_2-2" data-name="Layer 2">
+               <path class="cls-1" d="M266,36.12V236.88A136.16,136.16,0,0,1,173.5,273h-1.11A136.5,136.5,0,1,1,266,36.12Z"/>
+               <path class="cls-1" d="M266,226.61V337.39a72,72,0,1,1,0-110.78Z"/>
+               <path class="cls-1" d="M266,546.08V634H.22c-.15-2.65-.22-5.31-.22-8a145,145,0,0,1,266-79.92Z"/>
+            </g>
+         </g>
+      </svg>
+      <svg class="svg6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 350.5 266">
+         <defs>
+            <style>.cls-1{fill:#9f91da;opacity:0.15;}</style>
+         </defs>
+         <title>LowestLeft</title>
+         <g id="Layer_2" data-name="Layer 2">
+            <g id="Layer_2-2" data-name="Layer 2">
+               <path class="cls-1" d="M248.5,157.5a156.82,156.82,0,0,1-29.94,92.4A158.72,158.72,0,0,1,205.17,266H0V28.94A157.52,157.52,0,0,1,248,144.77Q248.5,151.07,248.5,157.5Z"/>
+               <path class="cls-1" d="M350.5,210a71.84,71.84,0,0,1-26.75,56h-90.5A72,72,0,1,1,350.5,210Z"/>
+            </g>
+         </g>
+      </svg>
+      <svg class="svg7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 438.5 720">
+         <defs>
+            <style>.cls-1{fill:#9f91da;opacity:0.15;}</style>
+         </defs>
+         <title>LGroupM</title>
+         <g id="Layer_2" data-name="Layer 2">
+            <g id="Layer_2-2" data-name="Layer 2">
+               <path class="cls-1" d="M438.5,249.5c0,107.33-67.76,198.82-162.84,234A249.44,249.44,0,0,1,0,412.38V86.62A248.89,248.89,0,0,1,189,0C326.8,0,438.5,111.7,438.5,249.5Z"/>
+               <circle class="cls-1" cx="151" cy="525.5" r="131.5"/>
+               <path class="cls-1" d="M139.5,648A72,72,0,0,1,0,673.1V622.9A72,72,0,0,1,139.5,648Z"/>
+            </g>
+         </g>
+      </svg>
+      <!-- END OF SVG-->
+      <!-- Home Content -->
+      <div id="home">
+         <div class="row">
+            <div class="col-lg-5">
+               <img class="vector1"src="images/Vector1.png" alt="Vec1" width="500px">
+            </div>
+            <div class="col-lg">
+               <h2 class="text-primary texthome">WELCOME TO GABRILLO DENTAL CLINIC</h2>
+               <p>Assign responsive-friendly margin or padding values to an element or a subset of its sides with shorthand classes. Includes support for individual properties, all properties, and vertical and horizontal properties. Classes are built from a default Sass map ranging from .25rem to 3rem.</p>
+               <button type="button" class="btn btn-outline-primary">Puntang Langit</button>
+            </div>
+         </div>
+      </div>
+      <div id="appointments">
+         <!-- Appointments Content -->
+         <div class="row">
+            <div class="col-lg-7">
+               <img class="vector2" src="images/Vector2.png" alt="vec2" width="300px">
+            </div>
+            <div class="col-lg">
+               <h2 class="text-primary textappointment">APPOINTMENTS</h2>
+               <p>Assign responsive-friendly margin or padding values to an element or a subset of its sides with shorthand classes. Includes support for individual properties, all properties, and vertical and horizontal properties. Classes are built from a default Sass map ranging from .25rem to 3rem.</p>
+               <button type="button" class="btn btn-outline-primary">SET APPOINTMENT</button>
+            </div>
+         </div>
+      </div>
+      <!-- AboutUsS Content -->
+      <div id="aboutus">
+         <div class="row">
+            <div class="col-lg-4 d-flex justify-content-center">
+               <div class="card bg-warning" style="width: 18rem;">
+                  <img class="card-img-top" src="images/Vector1.png" alt="Card image cap">
+                  <div class="card-body">
+                     <h5 class="card-title">VISION</h5>
+                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-4 d-flex justify-content-center">
+               <div class="card bg-info" style="width: 18rem;">
+                  <img class="card-img-top" src="images/Vector1.png" alt="Card image cap">
+                  <div class="card-body">
+                     <h5 class="card-title">MISSION</h5>
+                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-4 d-flex justify-content-center">
+               <div class="card bg-light" style="width: 18rem;">
+                  <img class="card-img-top" src="images/Vector1.png" alt="Card image cap">
+                  <div class="card-body">
+                     <h5 class="card-title">REVOLUTION</h5>
+                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                     <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+   </body>
 </html>
